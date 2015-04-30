115b15cd73676254432e36ffac011c10645b239b
Teun Zengerink <t.zengerink@gmail.com>
2015-04-30 10:19:02 +0200

    using user model when logging in

--- application/core.py
+++ application/core.py
@@ -3,12 +3,14 @@ from flask import Flask
 
 from application.views import login
 from application.errors import init_errors
+from application.models import init_models
 
 
 app = Flask(__name__)
 app.config.setdefault('SQLALCHEMY_DATABASE_URI', environ.get('DATABASE_URL'))
 
 init_errors(app)
+init_models(app)
 
 routes = {'/login': login}
 
