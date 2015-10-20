82954e26a969a0afc7cec11b24d903617b6c9559
Teun Zengerink <t.zengerink@gmail.com>
2015-10-20 16:22:35 +0200

    improved tests and their readability

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import pytest
 
 from alembic import command
 from alembic.config import Config
+from functools import partial
 
 from application.core import make_app
 from application.models import Item, User, db as _db, encode_token
@@ -12,8 +13,8 @@ os.environ['DATABASE_URL'] = 'sqlite:///:memory:'
 os.environ['JWT_SECRET'] = 'A_VERY_SECRET_KEY'
 
 
-@pytest.fixture(scope='session')
-def app(request):
+@pytest.yield_fixture(scope='session')
+def app():
     app = make_app({
         'TESTING': True,
         'SQLALCHEMY_DATABASE_URI': os.environ['DATABASE_URL'],
@@ -22,11 +23,9 @@ def app(request):
     ctx = app.app_context()
     ctx.push()
 
-    def teardown():
-        ctx.pop()
+    yield app
 
-    request.addfinalizer(teardown)
-    return app
+    ctx.pop()
 
 
 @pytest.fixture(scope='session')
@@ -34,8 +33,8 @@ def client(app):
     return TestClient(app)
 
 
-@pytest.fixture(scope='session')
-def db(app, request):
+@pytest.yield_fixture(scope='session')
+def db(app):
     _db.app = app
 
     connection = _db.engine.connect()
@@ -43,23 +42,23 @@ def db(app, request):
     config.attributes['connection'] = connection
     command.upgrade(config, 'head')
 
-    def teardown():
-        connection.close()
-        _db.drop_all()
+    yield _db
 
-    request.addfinalizer(teardown)
-    return _db
+    connection.close()
+    _db.drop_all()
 
 
 @pytest.fixture(scope='function')
 def items(db):
-    apple = Item(name='apple')
-    banana = Item(name='banana')
-    cucumber = Item(name='cucumber')
-    dill = Item(name='dill')
-    eggplant = Item(name='eggplant')
-    db.session.add_all([apple, banana, cucumber, dill, eggplant])
+    db.session.add_all([
+        Item(name='apple'),
+        Item(name='banana'),
+        Item(name='cucumber'),
+        Item(name='dill'),
+        Item(name='eggplant'),
+    ])
     db.session.commit()
+
     return Item.query.all()
 
 
@@ -68,8 +67,10 @@ def user(db):
     password = 'letmein'
     user = User(username='tester', password=password)
     user.plaintext_password = password
+
     db.session.add(user)
     db.session.commit()
+
     return user
 
 
@@ -78,15 +79,26 @@ class TestClient(object):
     def __init__(self, app):
         self.app = app
 
-    def open(self, http_method, url, headers={}, data=None, user=None,
-             assert_status_code=200, assert_headers={}):
-        headers = {'Content-Type': 'application/json'}
+    def __getattr__(self, name):
+        return partial(self._open, name)
+
+    def _open(self, http_method, url, headers=None, data=None, user=None,
+              assert_status_code=200, assert_headers=None):
+
+        if headers is None:
+            headers = {'Content-Type': 'application/json'}
+
         if user:
             headers['X-Auth-Token'] = encode_token(dict(id=user.id))
+
         client = self.app.test_client()
         method = getattr(client, http_method.lower())
         resp = method(url, headers=headers, data=data)
+
         assert resp.status_code == assert_status_code
-        for key, val in assert_headers:
-            assert resp.headers[key] == val
+
+        if assert_headers is not None:
+            for key, val in assert_headers:
+                assert resp.headers[key] == val
+
         return resp
--- tests/test_views.py
+++ tests/test_views.py
@@ -7,89 +7,87 @@ from application.models import Item
 
 
 def test_authenticated_forbidden(client):
-    client.open('get', '/item', assert_status_code=403)
+    client.get('/item', assert_status_code=403)
 
 
 class TestItem():
 
     def test_get(self, client, user, items):
         item = items.pop()
-        resp = client.open('get', '/item/{}'.format(item.id), user=user)
+        resp = client.get('/item/{}'.format(item.id), user=user)
         assert json.loads(resp.data.decode('utf-8')) == item.as_dict()
 
     def test_get_not_found(self, client, user):
-        client.open('get', '/item/{}'.format(99999), user=user,
-                    assert_status_code=404)
+        client.get('/item/{}'.format(99999), user=user, assert_status_code=404)
 
     def test_get_all(self, client, user, items):
-        resp = client.open('get', '/item', user=user)
+        resp = client.get('/item', user=user)
         for item in items:
-            assert item.as_dict() in json.loads(resp.data.decode('utf-8'))\
-                                         .get('items', [])
+            assert item.as_dict() in json.loads(
+                resp.data.decode('utf-8')).get('items', [])
 
     def test_post(self, client, user):
-        resp = client.open('post', '/item',
-                           data=json.dumps(dict(name='zucchini')), user=user)
+        resp = client.post('/item', data=json.dumps(
+            dict(name='zucchini')), user=user)
         data = resp.data.decode('utf-8')
         assert json.loads(data).get('id') > 0
         assert json.loads(data).get('name') == 'zucchini'
         assert json.loads(data).get('is_bought') is False
 
     def test_post_empty(self, client, user):
-        resp = client.open('post', '/item', user=user, assert_status_code=400)
+        resp = client.post('/item', user=user, assert_status_code=400)
         assert resp.status_code == 400
 
     def test_put(self, client, user, items):
         item = items.pop()
         assert item.is_bought is False
-        resp = client.open('put', '/item/{}'.format(item.id), user=user)
+        resp = client.put('/item/{}'.format(item.id), user=user)
         assert json.loads(resp.data.decode('utf-8')).get('is_bought') is True
 
     def test_put_not_found(self, client, user):
-        client.open('put', '/item/{}'.format(99999), user=user,
-                    assert_status_code=404)
+        client.put('/item/{}'.format(99999), user=user, assert_status_code=404)
 
     def test_put_empty(self, client, user):
-        client.open('put', '/item', user=user, assert_status_code=400)
+        client.put('/item', user=user, assert_status_code=400)
 
     def test_delete(self, client, user, items):
         item = items.pop()
-        client.open('delete', '/item/{}'.format(item.id), user=user)
+        client.delete('/item/{}'.format(item.id), user=user)
         assert item not in Item.query.all()
 
     def test_delete_not_found(self, client, user):
-        client.open('delete', '/item/{}'.format(99999), user=user,
-                    assert_status_code=404)
+        client.delete(
+            '/item/{}'.format(99999), user=user, assert_status_code=404)
 
     def test_delete_empty(self, client, user):
-        client.open('delete', '/item', user=user, assert_status_code=400)
+        client.delete('/item', user=user, assert_status_code=400)
 
 
 class TestLogin():
 
     def test_post(self, client, user):
-        data = json.dumps(dict(username=user.username,
-                               password=user.plaintext_password))
-        client.open('post', '/login', data=data)
+        data = json.dumps(dict(
+            username=user.username, password=user.plaintext_password))
+        client.post('/login', data=data)
 
     def test_post_forbidden(self, client):
         data = json.dumps({'incorrect': 'data'})
-        client.open('post', '/login', data=data, assert_status_code=403)
+        client.post('/login', data=data, assert_status_code=403)
 
 
 class TestStatus():
 
     def test_get(self, client):
-        client.open('get', '/status')
+        client.get('/status')
 
     @patch('application.models.db.session.query',
            return_value=sa.exc.TimeoutError('Connection timed out'))
     def test_get_exception(self, patched_method, client):
-        client.open('get', '/status')
+        client.get('/status')
         assert patched_method.call_count == 1
 
 
 class TestSuggest():
 
     def test_get(self, client, user):
-        client.open('get', '/suggest', user=user)
+        client.get('/suggest', user=user)
